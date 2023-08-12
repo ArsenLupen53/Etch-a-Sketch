800c80c6d83908ea7960d25165bdea6ad8c38a6b
ArsenLupen53 <kingnocktis@gmail.com>
2023-08-12 19:07:18 +0300

    Almost

--- script.js
+++ script.js
@@ -1,41 +1,59 @@
 const gridContainer = document.getElementById("grid-container");
-const defaultSize = 16;
+let defaultSize = 16;
 
 function createGrid(size) {
-    
+    clearGrid(); // Clear the grid before creating a new one
+    defaultSize = size;
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement("div");
         square.classList.add("grid-square");
         square.addEventListener("mouseover", changeColor);
         gridContainer.appendChild(square);
-        document.querySelector(".grid-square").style.width = "calc(960px / size)";
-        document.querySelector(".grid-square").style.height = "calc(960px / size)";
     }
+    setupGrid(size);
+}
+
+function setupGrid(size) {
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`
 }
 
 function changeColor(e) {
     e.target.style.backgroundColor = "black";
 }
 
+function changeWhite(e) {
+    e.style.backgroundColor = "white";
+}
 
-const resetButton = document.createElement("button");
-resetButton.textContent = "New Grid";
-document.body.insertBefore(resetButton, gridContainer);
+const gridButton = document.createElement("button");
+gridButton.textContent = "New Grid";
+document.body.insertBefore(gridButton, gridContainer);
 
-resetButton.addEventListener("click", () => {
-    const newSize = prompt("Enter the number of squares per side(max 100): ");
+gridButton.addEventListener("click", () => {
+    let newSize = prompt("Enter the number of squares per side (max 100): ");
+
     if (newSize > 0 && newSize <= 100) {
         createGrid(newSize);
     } else {
-        alert("Please enter a valid number between 1 and 100");
-        createGrid(defaultSize);
-
+        alert("Please enter a valid whole number between 1 and 100");
     }
-
 });
-
 function clearGrid() {
     while (gridContainer.firstChild) {
-        gridContainer.removeChild(gridContanier.firstChild);
+        gridContainer.removeChild(gridContainer.firstChild);
     }
 }
+
+
+const resetButton = document.createElement("button");
+resetButton.textContent = "Reset";
+document.body.insertBefore(resetButton, gridContainer);
+resetButton.addEventListener("click", function() {
+    const squares = document.querySelectorAll(".grid-square");
+    squares.forEach(function(square) {
+        changeWhite(square);
+    });
+});
+
+    
